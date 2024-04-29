6c937fb4c0321356a5219d2d3a064c78efb60290
Alleg <opankov@inbox.ru>
2024-04-29 12:13:04 +0300

    fixed

--- solution_1.py
+++ solution_1.py
@@ -4,19 +4,16 @@ def add_client(name, history):
     client_id = len(history)+1
     history[client_id] = {'name': name, 'orders': []}
 
-
-def Make_order(client_id, history, order_details):
+def make_order(client_id, history, order_details):
     history[client_id]['orders'].append(order_details)
-
 
 def get_history(client_id, history):
     return history[client_id]['orders']
-
 
 client_history: dict[str, int] = {}
 
 add_client('Roman', client_history)
 
-Make_order(1, client_history, {'order_id': 1, 'amount': 100})
+make_order(1, client_history, {'order_id': 1, 'amount': 100})
 
 print(get_history(1, client_history))
